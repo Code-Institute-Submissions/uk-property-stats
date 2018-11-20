c0e5bda29f18ef03eb85eae79569c864cfe0b8f4
James Shadrake <jimkarts@hotmail.co.uk>
2018-11-20 11:00:22 +0000

    Header sections for the prices and sales now included within the sales-charts section. Button and css updated accordingly

--- assets/js/graphButton.js
+++ assets/js/graphButton.js
@@ -4,19 +4,13 @@ $(document).ready(function() {
     $("#price-button").on("click", function() {
 		$(".price-charts").addClass("active");
 		$(".sales-charts").removeClass("active");
-		$("#price-section-title").addClass("active");
-		$("#sales-section-title").removeClass("active");
 	});
 	$("#sales-button").on("click", function() {
 		$(".price-charts").removeClass("active");
 		$(".sales-charts").addClass("active");
-		$("#price-section-title").removeClass("active");
-		$("#sales-section-title").addClass("active");
 	});
 	$("#hide-button").on("click", function() {
 		$(".price-charts").removeClass("active");
 		$(".sales-charts").removeClass("active");
-		$("#price-section-title").removeClass("active");
-		$("#sales-section-title").removeClass("active");
 	});
 });
